9f190275d3ff8e8f948d6b60eec1fe9b50521a48
Cathleen Pascal <c.pascal@interface3.be>
2026-08-26 16:10:17 +0200

    sauvegarde du fichier 'demonstration_orienté_objet_exercice1.php'

---
+++ demonstration_orienté_objet_exercice1.php
@@ -0,0 +1,32 @@
+Exercices : PHP Orienté Objet
+
+Exercice 01 - Analyser avant de coder
+
+Aucun code à écrire pour cet exercice, uniquement de la réflexion écrite.
+
+Dans les phrases suivantes, indiquez ce qui correspond à une classe et ce qui correspond à un objet :
+
+« Une voiture possède une marque et une couleur. »
+
+« Ma Clio rouge est garée devant chez moi. »
+- Objet, car on parle d'un objet précis en donnant une valeur à une caractéristique
+« Le compte bancaire de M. Dupont contient 250 €. »
+- Objet, car on parle d'un objet précis en donnant une valeur à une caractéristique
+« Un compte bancaire permet de déposer et de retirer de l'argent. »
+- Classe, car on parle des comportements d'un compte bancaire en général
+
+On souhaite modéliser un téléphone portable. Proposez :  
+3 propriétés (des données) : marque, modèle, taille...
+2 méthodes (des actions) : telephoner(), envoyer_sms()...
+
+Question de vocabulaire : quelle est la différence entre une propriété et une méthode ?
+
+a - classe (global)
+b - objet (spécifique)
+c - objet (spécifique)
+d - classe (global)
+
+propriétés : marque, nom, taille
+méthodes : appel, message ?
+
+une propriété est une caractéristique  qui peut-être commune à l'ensemble des objets, une méthode va définir le comportement de l'objet
