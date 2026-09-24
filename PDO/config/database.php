c24aad2244022b77a3cd9705f804d63bc1eedd08
Vasumitra <pascal.cathleen1989@gmail.com>
2026-09-24 09:56:08 +0200

    ajout des informations de connexion à la DB et de la tentative de connexion (try/catch)

--- PDO/config/database.php
+++ PDO/config/database.php
@@ -2,20 +2,20 @@
 
 // Informations de connexion à la base de données :
 
-$source = "sqlsrv";
+$source = "sqlserver";
 $host = "localhost";
 $dbname = "";
 
-$dsn = "$source:Server=$host;Database=$dbname;TrustServerCertificate=true"; // à détailler...
+$dsn = "$source:Server=$host;Database=$dbname;TrustServerCertificate=true"; // à détailler
 $user = "";
 $pass = "";
 $options = [];
 
-// Tentative de connexion :
+// tentative de connexion :
 
-try {
-$pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOExeption $e) { // $e = variable contenant le message d'erreur
-die("Erreur de connexion : " . $e->getMe);
+try{
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) { // $e = variable contenant le message d'erreur
+die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
