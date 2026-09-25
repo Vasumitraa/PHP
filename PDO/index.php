dba9649605c6a869dbbb196e4e5aa9ffd38407f0
Vasumitra <pascal.cathleen1989@gmail.com>
2026-09-25 15:20:16 +0200

    mise à jour du fichier 'index.php'

--- PDO/index.php
+++ PDO/index.php
@@ -11,7 +11,12 @@ $routes = [
 'books' => [
     'file' => 'pages/books/read.php',
     'title' => 'Liste des livres',
-]
+],
+
+'book-details' => [
+    'file' => 'pages/books/read.php',
+    'title' => 'Liste des livres',
+],
 
 ];
 
