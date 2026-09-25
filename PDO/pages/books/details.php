dafa515a1d71c8b8ddbcf66f524b440796d77e07
Vasumitra <pascal.cathleen1989@gmail.com>
2026-09-25 15:17:16 +0200

    récupération des détails des livres et des auteurs via une requête SQL

--- PDO/pages/books/details.php
+++ PDO/pages/books/details.php
@@ -1,5 +1,18 @@
 <?php
 
+$sql = "SELECT  l.id,
+                l.titre,
+                l.annee,
+                l.prix,
+                l.isbn,
+                a.id AS auteur_id,
+                a.nom AS auteur_nom,
+                a.prenom AS auteur_prenom,
+                a.nationalite AS auteur_nationalite,
+        
+        FROM livre AS l
+            JOIN auteur AS a ON l.auteur_id = a.id";
+
 ?>
 
 <h1>Détails du livre</h1>
