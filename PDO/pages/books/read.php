e00342d1b391564a09f528cd66e095a8ee5e9dab
Vasumitra <pascal.cathleen1989@gmail.com>
2026-09-25 14:36:30 +0200

    ajout de l'itération sur les titres de livres

--- PDO/pages/books/read.php
+++ PDO/pages/books/read.php
@@ -1,8 +1,31 @@
 <?php
 
+// Récupérer la liste des livres triés par titre :
+$sql = "SELECT id, titre 
+        FROM livre
+        ORDER BY titre";
 
+$livres = $pdo->query($sql)->fetchAll();
 
 ?>
 
 <h1>Liste des livres</h1>
 
+<p><?= count($livres) ?> livre(s)</p> <!--pas nécessaire dans mon projet-->
+
+<div class="cartes">
+
+<?php foreach($livres as $livre) ?>
+    
+<article class="carte">
+    <h2><?= $livre["titre"] ?></h2>
+
+    <div class="actions">
+        <a href="index.php?page=book-details&amp;id=<?=  $livre['id'] ?>">Détails</a>
+    </div>
+
+</article>
+
+<?php endforeach ?>
+
+</div>
